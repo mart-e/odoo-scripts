8ca4d24ab033e1bfd432db30ee80e97d4afc0efe
Martin Trigaux <me@mart-e.be>
2018-06-26 11:07:15 +0200

    [ADD] use github code search api
    And play with dotenv library

---
+++ github/code_search.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os
+import requests
+
+from requests.auth import HTTPBasicAuth
+from pprint import pprint
+
+# username/password stored in .env file
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+
+BASE_URL = "https://api.github.com/search/code"
+
+SEARCH_CRITERIAS = [
+    'odoo',
+]
+
+AUTH = HTTPBasicAuth(os.getenv('GITHUB_USERNAME'), os.getenv('GITHUB_PASSWORD'))
+
+
+def rget(url, **kw):
+    """ Make a GET request on a given URL """
+    res = requests.get(url, auth=AUTH, **kw)
+    if res.headers.get('x-ratelimit-remaining') == '0':
+        print("Hit rate limit!")
+        return None
+    return res.json()
+
+
+def rpost(url, data, **kw):
+    """ Make a POST request on a given URL """
+    res = requests.post(url, json=data, auth=AUTH, **kw)
+    if res.headers.get('x-ratelimit-remaining') == '0':
+        print("Hit rate limit!")
+        return None
+    return res.json()
+
+
+found = {}
+for search in SEARCH_CRITERIAS:
+    search_res = rget(BASE_URL, params={'q': search, 'type': 'Code'})
+    if not search_res['total_count']:
+        continue
+
+    for item in search_res['items']:
+        rurl = item['repository']['html_url']
+        if rurl.startswith("https://github.com/odoo/"):
+            continue
+
+        found.setdefault(rurl, [0, set()])
+        found[rurl][0] += 1
+        found[rurl][1].add(search)
+
+sorted_matches = sorted(found.items(), key=lambda r: r[1][0], reverse=True)
+for match in sorted_matches:
+    print(f"{match[0]}\n\tmatched {match[1][0]} time(s) ({', '.join(match[1][1])})")
