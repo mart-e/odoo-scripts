60b96c9266143183c7aff28a810d4884c26828bd
Martin Trigaux <mat@odoo.com>
2019-08-30 15:15:04 +0200

    Transifex: Update merge-po

--- transifex/merge-po.py
+++ transifex/merge-po.py
@@ -1,53 +1,75 @@
 #!/usr/bin/env python2
+# usage:
+#  $ python merge-po.py --ref $REF --trad $SRC --dest $DEST
+# where - $REF is where the reference .pot is stored
+#       - $SRC is where the translations to use are stored
+#       - $DEST is where to store the new translations
+#       ($REF and $DEST are probably the same)
+#       arguments are path to source repositories
 
 import argparse
 import os
 import subprocess
 from os.path import join as j
 
-ADDONS_SUBPATHS = ['', 'addons', 'openerp/addons']
-MASTER_LANG = ['nl', 'zh_CN', 'fr', 'hu', 'pt_BR', 'es']
-
+ADDONS_SUBPATHS = ['', 'addons', 'openerp/addons', 'odoo/addons']
 
 def merge_po(src_path, new_path, dest_path):
+
+    def guess_openerp(firstpart, file, addons_subpath, addon):
+        guesspath = j(firstpart, addons_subpath, addon, 'i18n', file)
+        if addons_subpath == 'openerp/addons' and not os.path.exists(guesspath):
+            return j(firstpart, 'odoo/addons', addon, 'i18n', file)
+        elif addons_subpath == 'odoo/addons' and not os.path.exists(guesspath):
+            return j(firstpart, 'openerp/addons', addon, 'i18n', file)
+        else:
+            return guesspath
+
     for addons_subpath in ADDONS_SUBPATHS:
         if not os.path.exists(j(dest_path, addons_subpath)):
             print("skip subpath '%s'..." % j(dest_path, addons_subpath))
             continue
 
         for addon in os.listdir(j(dest_path, addons_subpath)):
+
             i18n_path = j(dest_path, addons_subpath, addon, 'i18n')
             if not os.path.exists(i18n_path):
                 print("skip '%s'..." % i18n_path)
                 continue
 
-            print("Processing '%s'..." % addon)
+            print("Processing '%s'... in %s" % (addon, i18n_path))
+            # for lang in sorted(filter(lambda x: x == 'de.po', os.listdir(i18n_path))):
             for lang in sorted(filter(lambda x: x.endswith('.po'), os.listdir(i18n_path))):
-                src = j(src_path, addons_subpath, addon, 'i18n', lang)
-                new = j(new_path, addons_subpath, addon, 'i18n', lang)
-                dest = j(dest_path, addons_subpath, addon, 'i18n', lang)
-                ref = j(dest_path, addons_subpath, addon, 'i18n', addon+'.pot')
-                language = lang.replace('.po', '')
+
+                guess = lambda a, b,: guess_openerp(a, b, addons_subpath, addon)
+                src = guess(src_path, lang)
+                new = guess(new_path, lang)
+                dest = guess(dest_path, lang)
+                ref = guess(dest_path, addon+'.pot')
+
+                # language = lang.replace('.po', '')
+                # print("   %s: %s (%s) -> %s (%s)" % (language, src, os.path.exists(src), new, os.path.exists(new)))
                 if not os.path.exists(src):
                     subprocess.call(['cp', new, dest])
-                elif not os.path.exists(new) or language not in MASTER_LANG:
+                elif not os.path.exists(new):
                     subprocess.call(['cp', src, dest])
                 else:
                 # cmd = 'msgcat %(src)s %(new)s -o %(dest)s'
-                    subprocess.call(['msgcat', '--use-first', src, new, '-o', dest])
-                cmd = "msgmerge --no-fuzzy-matching -q %(dest)s %(ref)s | msgattrib --no-obsolete -o %(dest)s" % {'ref': ref, 'dest': dest}
+                    subprocess.call(['msgcat', '--no-wrap', '--use-first', src, new, '-o', dest])
+                    subprocess.call(['msgattrib', '--translated', '--no-fuzzy', '--no-obsolete', dest, '-o', dest])
+                cmd = "msgmerge --no-wrap --no-fuzzy-matching -q %(dest)s %(ref)s | msgattrib --no-fuzzy --no-obsolete -o %(dest)s" % {'ref': ref, 'dest': dest}
                 subprocess.call(cmd, shell=True)
                 # subprocess.call(['msgmerge', dest, ref, '--update', '-N'])
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base', required=True,
-                        help='path to source directory')
-    parser.add_argument('--new', required=True,
-                        help='path to new translations')
+    parser.add_argument('--ref', required=True,
+                        help='path to source directory where the pot is defined')
+    parser.add_argument('--trad', required=True,
+                        help='path to reference translations')
     parser.add_argument('--dest', required=True,
                         help='path where generated po will be stored')
 
     args = parser.parse_args()
-    merge_po(args.base, args.new, args.dest)
+    merge_po(args.ref, args.trad, args.dest)
